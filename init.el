7d45e8a25ce8933eda8843fe79332c41288e4875
Dave Loyall <david.loyall@nebraska.gov>
2016-05-17 16:44:18 -0500

    initial init

---
+++ init.el
@@ -0,0 +1,5 @@
+;GNU Emacs 25.1.50.1 (x86_64-pc-linux-gnu, GTK+ Version 3.14.5) of 2016-05-16
+;in use on metalbaby.stone.ne.gov
+
+(add-to-list 'load-path "~/.emacs.d/site-lisp/graphviz-dot-mode")
+(load "graphviz-dot-mode.el")
